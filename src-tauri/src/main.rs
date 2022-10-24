4377e8a034cdf31a10f3f45a787f0bb2746b3cba
Anthony Wilkins <awilkins@strongholddm.com>
2022-10-24 13:10:19 -0400

    Add error for empty pool

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -134,6 +134,9 @@ async fn scan_miner(client: Client, ip: String) -> Miner {
                 }
             }
         }
+        if ret.pools[0].url.is_empty() {
+            ret.errors.push("No pool set".to_string());
+        }
     }
     ret
 }
