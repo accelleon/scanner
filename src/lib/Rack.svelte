895505008961b36797cdaea8efb4909cd8158d0f
Anthony Wilkins <awilkins@strongholddm.com>
2022-10-28 13:16:20 -0400

    Select/deselect whole racks

--- src/lib/Rack.svelte
+++ src/lib/Rack.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
   import type { Rack } from "../types";
   import Miner from "./Miner.svelte";
+  import _ from 'lodash';
+
   export let rack: Rack = undefined;
   export let selection: any[] = undefined;
   let ncols = rack.miners[0].length;
+
+  function selectAll(e: any) {
+    let new_selection = rack.miners.flat(2).filter((miner) => miner.make);
+    // if ctrl is held we want to add/remove
+    // otherwise toggle it
+    if (e.ctrlKey) {
+      // check if the rack is already selected
+      if (_.intersection(selection, new_selection).length === new_selection.length) {
+        // remove it
+        selection = _.difference(selection, new_selection);
+      } else {
+        // add it
+        selection = _.union(selection, new_selection);
+      }
+    } else {
+      // toggle it on/off
+      if (_.isEqual(selection, new_selection)) {
+        selection = [];
+      } else {
+        selection = new_selection;
+      }
+    }
+  }
 </script>
 
-<div class="container">
+<div class="container" on:dblclick={selectAll}>
   {rack.name}
   <div class="grid" style="--ncols: {ncols}">
     {#each rack.miners as row}
